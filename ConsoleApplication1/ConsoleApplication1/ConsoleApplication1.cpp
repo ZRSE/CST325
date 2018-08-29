547063556100c283bc004b3d748f95ecda46d8b5
Zakarias Roese <roes6537@csumb.ad.csumb.edu>
2018-08-29 11:33:36 -0700

    My first C++ Hello World Program

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+/**
+* Author: Zakarias Roese
+* For class: CST325
+* Comment: This is a simple Hello World program
+* Date: 08/29/2018
+*/
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "hello world";
+    return 0;
+}
+
